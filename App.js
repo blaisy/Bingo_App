abfd4f790aa34610dc2d8949ec2cc0e3a0f183fd
Elia Todescato <eliatodescato@Elias-MacBook-Pro.local>
2023-08-01 23:57:22 +0200

    feat(app): Aggiunta pagina di login
    Aggiunta pagina di login come componenti

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//qeusta renderizza
+import Login from './src/components/login/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className = "grid h-screen place-items-center">
+      <View className="bg-green-500">
+        <Login />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
