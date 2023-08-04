5c3bceffcdd4ce104b09d557ab8d0dc455e057ab
Elia Todescato <eliatodescato@Elias-MacBook-Pro.local>
2023-08-04 20:26:56 +0200

    feat(app): Updated Login Interface

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,7 @@
 import { View, Text, Image, Button } from 'react-native';
 import React from 'react';
 import EcoButton from '../../Utils/EcoButton';
+import { ImageBackground } from 'react-native';
 /**
  * This is the main core of the component Login.
  * It renders the login form. It also check if the user is logged in or not and retrieve 
@@ -23,34 +24,36 @@ export default class Login extends React.Component {
     render() {
         return (
             <View>
-                <View className="bg-green-900 flex items-center justify-center h-screen ">
-                    <View className=" text-white  pb-10  ">
-                        <Image className=" h-60 w-60 rounded-full"
-                            source={{
-                                uri: 'https://images.unsplash.com/photo-1559509546-4f2fdd5d5825?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=686&q=80',
-                            }} />
+                <ImageBackground
+                    source={{ uri: 'https://images.unsplash.com/photo-1612982228462-180baddf0d41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80' }}
+                    blurRadius={10}>
+                    <View className=" flex items-center justify-center h-screen ">
+
+                        <View className=" text-white  pb-10  ">
+
+                            <Image className=" h-60 w-60 rounded-full"
+                                source={{
+                                    uri: 'https://cdn.dribbble.com/users/2049961/screenshots/10492508/media/36da55197af8c90837e6583bec89fb1a.gif',
+                                }} />
+
+                        </View>
+
+                        <View className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                            <Button title='Sign-In with email' color={"white"}></Button>
+                        </View>
+
+                        {/* This button have to interface the google client */}
+                        <View className="pt-10 flex flex-row">
+                            <EcoButton txt="Google" bg="white" color="black" />
+                            <EcoButton txt="AppleId" bg="white" color="black" />
+                        </View >
+
                     </View>
-
-                    <View className="text-white pb-10 ">
-                        <Text className="text-white text-lg text-justify ">The only ECOproductivity app you need</Text>
-                    </View>
-
-                    <View className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                        <Button title='Sign-In with email' color={"white"}></Button>
-                    </View>
-
-                    {/* This button have to interface the google client */}
-                    <View className="pt-10 flex flex-row">
-                        <EcoButton txt="Google"  bg = "white" color="white"/>
-                        <EcoButton txt="AppleId" bg = "white" color="white"/>
-                    </View >
-
-                </View>
+                </ImageBackground>
 
             </View>
         );
     }
-    // <EcoButton txt = "test" bg = "red"></EcoButton>
 
     handleChange(event) {
         this.setState({
